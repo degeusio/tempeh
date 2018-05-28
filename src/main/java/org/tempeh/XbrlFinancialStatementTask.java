cef02577241dadc12794b9862cdd38d3a1495012
tbonza <tylers.pile@gmail.com>
2018-05-28 09:13:20 -0400

    replicated functionality but found a dead link given in the main method

--- src/main/java/org/tempeh/XbrlFinancialStatementTask.java
+++ src/main/java/org/tempeh/XbrlFinancialStatementTask.java
@@ -11,51 +11,50 @@ import org.tempeh.xbrl.report.FinancialStatementsReport;
 import org.xml.sax.InputSource;
 
 
-public class XbrlFinancialStatementTask{
-
-	private final IFileCache fileCache;
-	private String xbrlInstanceUri;
+public class XbrlFinancialStatementTask {
+    
+    private final IFileCache fileCache;
+    private String xbrlInstanceUri;
+    
+    public XbrlFinancialStatementTask(IFileCache fileCache, String xbrlInstanceUri){
+	this.fileCache = fileCache;
+	this.xbrlInstanceUri = xbrlInstanceUri;
+    }
+    
+    public void runTask() throws Exception {
 	
-	public XbrlFinancialStatementTask(IFileCache fileCache, String xbrlInstanceUri){
-		this.fileCache = fileCache;
-		this.xbrlInstanceUri = xbrlInstanceUri;
+	Util util = new Util();
+	XbrlLoader loader = new XbrlLoader(fileCache);
+	XbrlInstance instance = null;
+	HttpURLConnection conn = null;
+	try{
+	    conn = util.fetchUrl(xbrlInstanceUri);
+	    if(conn == null){
+		throw new TempehException("Unable to get URL: " + xbrlInstanceUri);
+	    }
+	    
+	    instance = loader.load(new URI(xbrlInstanceUri),
+				   new InputSource(conn.getInputStream()));
+	}
+	catch(Exception e){
+	    System.out.println(e);
+	    throw new TempehException("Error parsing xbrl instance: " + xbrlInstanceUri, e);
+	}
+	finally{
+	    if(conn != null)
+		conn.disconnect();
 	}
 	
-	public void runTask() throws Exception {
-
-		Util util = new Util();
-		XbrlLoader loader = new XbrlLoader(fileCache);
-		XbrlInstance instance = null;
-		HttpURLConnection conn = null;
-		try{
-			conn = util.fetchUrl(xbrlInstanceUri);
-			if(conn == null){
-				throw new TempehException("Unable to get URL: " + xbrlInstanceUri);
-			}
-			
-			instance = loader.load(new URI(xbrlInstanceUri), new InputSource(conn.getInputStream()));
-		}
-		catch(Exception e){
-			throw new TempehException("Error parsing xbrl instance: " + xbrlInstanceUri, e);
-		}
-		finally{
-			if(conn != null)
-				conn.disconnect();
-		}
-		
-		FinancialStatementsReport report = new FinancialStatementsReport(instance);
-		buildFinancialStatements(report, instance);
-	}
+	FinancialStatementsReport report = new FinancialStatementsReport(instance);
+	buildFinancialStatements(report, instance);
+    }
+    
+    private void buildFinancialStatements(FinancialStatementsReport report, XbrlInstance instance)
+	throws Exception{
 	
-	private void buildFinancialStatements(FinancialStatementsReport report, XbrlInstance instance) throws Exception{
-		
-		for(PresentationLink presLink : instance.getPresentationLinks().values()){
-			if(report.willHandlePresentation(presLink))
-				report.buildPresentationLink(presLink);
-		}
-		
-	}
-	
-
-	
+	for(PresentationLink presLink : instance.getPresentationLinks().values()){
+	    if(report.willHandlePresentation(presLink))
+		report.buildPresentationLink(presLink);
+	}	
+    }
 }
